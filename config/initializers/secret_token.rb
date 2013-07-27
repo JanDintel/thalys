c320cac72b5b98631500d2b846c709ca8dbd603e
Job van der Voort <jobvandervoort@gmail.com>
2013-07-27 14:35:52 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thalys::Application.config.secret_key_base = 'eae15d3f79c215d120c290dc131b3bd16d41f3803d93b17f93ae03bd0191f9d2c59fb885245a63c8d8b5f9b69e0b3e999b2e274b75f3a265203e1db1898ebd2e'
