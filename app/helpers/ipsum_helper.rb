905cd1a2d4aaaa024e9b8016a691117c786adb5b
Job van der Voort <jobvandervoort@gmail.com>
2013-08-28 21:14:33 +0200

    removals

--- app/helpers/ipsum_helper.rb
+++
@@ -1,5 +0,0 @@
-module IpsumHelper
-  def ipsum
-    return 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nullam id dolor id nibh ultricies vehicula ut id elit. Nulla vitae elit libero, a pharetra augue. Cras mattis consectetur purus sit amet fermentum.'
-  end
-end
